40ee20e9aa4258be1f4f99dbdc9001a091c73bb4
long-Iceman <long_Iceman@outlook.com>
2019-01-20 18:14:41 +0800

    Update auto_login.py

--- auto_login.py
+++ auto_login.py
@@ -27,7 +27,7 @@ class INST(object):
         return result
         
     def _login(self, request):
-        login_url = 'https://drcom.szu.edu.cn/a70.htm'
+        login_url = 'https://drcom.szu.edu.cn/a70.htm' # address for certification
         header = {
             'Host': 'drcom.szu.edu.cn',
             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
